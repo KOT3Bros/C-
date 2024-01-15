57043e0639297241af9f070a38632678c585b3cd
Pavel Buryakovsky <buryakovsky.pavel@gmail.com>
2024-01-15 22:04:54 +0500

    add seminar3

---
+++ Seminar3/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и делятся нацело на 7.
+// Пример [1 5 11 21 81 4 0 91 2 3] => 2
+
+int[] CreateArray(int size, int minRange, int maxRange)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minRange, maxRange + 1);
+    }
+    return array;
+}
+
+int GetCount(int[] arr)
+{
+    int count = 0;
+    foreach (var number in arr)
+    {
+        if (number % 7 == 0 && number % 10 == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(N, 0, 100);
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+Console.WriteLine($"Total: {GetCount(array)}");
---
+++ Seminar3/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Заполните массив на N (вводится с консоли, не более 8) случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из массива. Старший разряд числа находится на 0-м индексе, младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+int ConvertArrayToInt(int[] array)
+{
+    int result = 0;
+    for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+    {
+        result = result + array[i] * (int)Math.Pow(10, j);
+    }
+    return result;
+}
+
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(N);
+if (array[0] == 0 || N > 8)
+{
+    Console.WriteLine("Размер массива больше 8 элементов или первый элемент равен 0");
+    return;
+}
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+Console.WriteLine($"Число: {ConvertArrayToInt(array)}");
